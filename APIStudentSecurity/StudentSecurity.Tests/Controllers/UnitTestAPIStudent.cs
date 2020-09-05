1c6c39df28979ef96dec6497541db4c86c73caad
ENRIQUE NUÑEZ CESPEDES <a2019112540@estudiantes.upsa.edu.bo>
2020-09-05 13:17:37 -0400

    Creacion de UnitTest Web APIStudent

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTestAPIStudent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTestAPIStudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            // ARRANGE
+            StudentsController studentsController = new StudentsController();
+            // ACT
+            var listaEstudiantes = studentsController.GetStudents();
+            // ASSERT
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
